af88611e14aa513c55034bedcad312ece5650081
Vivek Garg <114947617+gargvivek2003@users.noreply.github.com>
2023-04-22 11:46:28 +0530

    signup page added

--- SignUp.js
+++ SignUp.js
@@ -1,3 +1,21 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/mydatabase', { useNewUrlParser: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
+
+  const MyModel = mongoose.model('MyModel', mySchema);
+  
+  const mySchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    number:integer,
+    password:String,
+  });
+
+
+  
+
 $(".input_text").focus(function(){
     $(this).prev('.fa').addclass('glowIcon')
 })
--- login.js
+++ login.js
@@ -4,3 +4,51 @@ $(".input_text").focus(function(){
 $(".input_text").focusout(function(){
     $(this).prev('.fa').removeclass('glowIcon')
 })
+
+function validate() { 
+    var name = document.reg_form.name;     
+    var email = document.reg_form.email;         
+    var gender = document.reg_form.gender; 
+    var lang = document.reg_form.lang;       
+    if (fname.value.length <= 0) {    
+        alert("User ID is required");    
+        fname.focus();    
+        return false;    
+    }    
+    if (lname.value.length <= 0) {    
+        alert("Password is required");    
+        lname.focus();    
+        return false;    
+    }    
+    if (name.value.length <= 0) {    
+        alert("Name is required");    
+        name.focus();    
+        return false;    
+    }    
+if (Country.value == "(Please select a country)") {    
+        alert("Country Name is required");    
+        Country.focus();    
+        return false;    
+    }    
+if (Code.value.length <= 0) {    
+        alert("ZIP Code is required");    
+        Code.focus();    
+        return false;    
+    }    
+if (email.value.length <= 0) {    
+        alert("Email Id is required");    
+        email.focus();    
+        return false;    
+    }    
+    if (gender.value.length <= 0) {    
+        alert("Gender is required");    
+        gender.focus();    
+        return false;    
+    }    
+    if (lang.value.length <= 0) {    
+        alert("Language is required");    
+        lang.focus();    
+        return false;    
+    }    
+    return false;    
+}    
